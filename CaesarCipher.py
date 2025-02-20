69c56972a825fcecd6b6ccf71394249c0fafa4fa
Jschnakenberg06 <jonasschnakenberg@unomaha.edu>
2025-02-20 18:19:16 +0000

    Done with Lab 5

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -10,26 +10,40 @@ def encode(message, key):
     secret = ""
 
     for letter in message:
-        if (alpha.find(letter) >= 0): #check to see if the letter is actually a letter
+        if letter in alpha:  
             spot = (alpha.find(letter) + key) % 26
-            secret = secret + alpha[spot]
-        else: # letter must have been a number, symbol, or punctuation.
-            secret = secret + letter
+            secret += alpha[spot]
+        else:  
+            secret += letter
 
     return secret
 
-#def decode(message, key):
-    #We will want to decode the message here.
+
+def decode(message, key):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  
+    message = message.upper()
+    plaintext = ""
+
+    for letter in message:
+        if letter in alpha:
+            spot = (alpha.find(letter) - key) % 26
+            plaintext += alpha[spot]  
+        else:
+            plaintext += letter  
+
+    return plaintext
+
 
 def main():
     message = input("Enter a message: ")
     key = int(input("Enter a key: "))
 
     secret = encode(message, key)
-    print ("Encrypted:", secret)
-    #plaintext = decode(secret, key)
-    #print ("Decrypted:", plaintext)
+    print("Encrypted:", secret)
+
+    plaintext = decode(secret, key)
+    print("Decrypted:", plaintext)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
--- WordGame.py
+++ WordGame.py
@@ -4,37 +4,69 @@ import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
-    return False
+    return letter in word
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
-    return False
+    return letter == word[spot]
 
 def rateGuess(myGuess, word):
     """Rates your guess and returns a word with the following features.
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
+    feedback = ["*"] * 5
+    word_used = list(word)
 
+    # Check for correct letters in the correct spot (uppercase)
+    for spot in range(5):
+        myLetter = myGuess[spot]
+        if inSpot(myLetter, word, spot):
+            feedback[spot] = myLetter.upper()
+            word_used[spot] = None  # Mark as used
+
+    # Check for correct letters in the wrong spot (lowercase)
+    for spot in range(5):
+        myLetter = myGuess[spot]
+        if feedback[spot] == "*":
+            if myLetter in word_used:
+                feedback[spot] = myLetter.lower()
+                word_used[word_used.index(myLetter)] = None  # Mark as used
+
+    return "".join(feedback)
 
 def main():
-    #Pick a random word from the list of all words
-    wordFile = open("words.txt", 'r')
-    content = wordFile.read()
+    # Pick a random word from the list of all words
+    with open("words.txt", 'r') as wordFile:
+        content = wordFile.read().strip()
     wordList = content.split("\n")
     todayWord = random.choice(wordList)
-    print(todayWord)
 
-    #User should get 6 guesses to guess
+    guessNum = 1
+    while guessNum <= 6:
+        # Ask user for their guess
+        validWord = False
+        while not validWord:
+            guess = input("Enter Guess: ").strip().lower()
+            if guess not in wordList:
+                print("Word not in list. Try again.")
+            elif len(guess) != 5:
+                print("Please enter a 5-letter word.")
+            else:
+                validWord = True
 
-    #Ask user for their guess
-    #Give feedback using on their word:
+        # Provide feedback
+        feedback = rateGuess(guess, todayWord)
+        print(feedback)
 
+        if guess == todayWord:
+            print(f"You are correct!! {guessNum} Tries!")
+            return  # Exit the function if guessed correctly
 
+        guessNum += 1
 
-
+    print("Game over! The word was", todayWord)
 
 if __name__ == '__main__':
-  main()
+    main()
+
